894cca03497ff05ad09c965a707c8046c6172450
Brumens <68474266+Brum3ns@users.noreply.github.com>
2022-01-27 13:18:23 +0100

    Create lfi_limited.py

---
+++ lfi_limited.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys, urllib.parse
+
+input = urllib.parse.unquote(str(sys.argv[1]))  
+
+#Only possible to open txt files inside the default directory:
+#Therefore delete all "/" and "."
+def filter(input):
+    input = input.replace('/', '').replace('.', '')
+    input = str(input) + '.txt'
+
+    return urllib.parse.unquote(input)
+
+try:
+    filename = open(filter(input), 'r')
+
+    print('::File content:')
+    for l in filename:
+        l = l.replace('\n', '')
+        print(l)
+except:
+    print(':: This file do not exist!')
+
+#Delete memory by killing the script
+globals().clear()
+sys.exit()
